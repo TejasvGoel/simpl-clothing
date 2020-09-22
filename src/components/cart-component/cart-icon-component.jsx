b9615a33d46cc894a445a3fcbd2e7e35bd1af558
Tejasv Goel <tejasvgoel@gmail.com>
2020-09-22 11:57:14 -0400

    added selectors, reduced rendering

--- src/components/cart-component/cart-icon-component.jsx
+++ src/components/cart-component/cart-icon-component.jsx
@@ -4,21 +4,26 @@ import {ReactComponent as ShoppingIcon } from '../../assets/shopping-icon.svg'
 
 import { connect } from 'react-redux';
 import { HiddenDropdown } from '../../redux/cart-toggle/cart.action';
+import {selectCartItemsCount} from '../../redux/cart-toggle/cart.selectors';
 
 import './cart-component-style.scss';
 
-const CartIcon = ({ HiddenDropdown }) =>{
+const CartIcon = ({ HiddenDropdown, itemCount }) =>{
     return(
         <div className='cart-icon' onClick={HiddenDropdown}>
             <ShoppingIcon  className='shopping-icon'/>
-            <span className='item-count'> 0 </span>
+            <span className='item-count'> {itemCount} </span>
 
         </div>
     );
 }
 
+const mapStateToProps = state =>({
+    itemCount: selectCartItemsCount(state)
+})
+
 const mapDispatchToProps = dispatch =>({
  HiddenDropdown: () => dispatch(HiddenDropdown())
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
